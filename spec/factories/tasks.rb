108a200f79de8ba4d627f1b2a0528f80fc608cee
hado anselme <anselmehado85@yahoo.com>
2021-06-20 12:47:42 +0100

    exemple

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:name) { |n| "TEST_NAME#{n}"}
+    sequence(:content) { |n| "TEST#{n}content"}
+    sequence(:deadline) { |n| "TEST_NAME#{n}"}
+    sequence(:status) { |n| "TEST#{n}status"}
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,9 +11,13 @@ RSpec.describe Task, type: :model do
           expect(task).not_to be_valid
         end
       end
+
   　　context 'les details sont vides' do
         it 'il est pris dans la validation' do
         @task.name = ''
+        @task.content = ''
+        @task.deadline = ''
+        @task.status = ''
         expect(@task.valid?).to eq(false)
         end
       end
