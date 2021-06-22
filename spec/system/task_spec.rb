adf362491c576526a598ce721ca549932d186cec
hado anselme <anselmehado85@yahoo.com>
2021-06-22 05:49:04 +0100

    test

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
-RSpec.describe 'Task management function', type: :system do
-  describe 'List display function' do
-    context 'When transitioning to the list screen' do
-      it 'The created task list is displayed' do
-        # step1で実装済みのためomit
+RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task, title: 'task') }
+  before do
+    # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
+    visit tasks_path
+  end
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される'
+        # タスクデータ作成と一覧画面への遷移の記述を移動したため、ここでの記述は不要
+        # step1で実装済みのため省略
       end
     end
-    # test内容を追加で記載する
-    context 'When tasks are arranged in descending order of creation date and time' do
-      it 'New task is displayed at the top' do
-        # Implement here
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        # ここに実装する
       end
     end
   end
+end
