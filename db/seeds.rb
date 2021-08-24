74b8864e573b8570b90396aac7bb142566923abc
hado anselme <anselmehado85@yahoo.com>
2021-08-24 13:24:34 +0100

    digest

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-#
 
 
 # User.create(
-#     [name: "userY",
-#     email: "userY@yahoo.com",
+#     [name: "jonas",
+#     email: "jonas@yahoo.com",
 #     password_digest: "JETroot1982",
 #     password_confirmation: "JETroot1982",
-# 		admin: false]
+# 		admin: 'true']
 # )
 
 
-
-User.create(
-    [name: "jonas",
-    email: "jonas@yahoo.com",
-    password_digest: "JETroot1982",
-    # password_confirmation: "JETroot1982",
-		admin: true]
-)
+User.create(name: "anselme", email: "anselme@yahoo.fr", admin: "true", password: "123456", password_confirmation: "123456")
