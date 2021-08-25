2527ec712d74cfc1b85b9dbecef500c9b3527663
hado anselme <anselmehado85@yahoo.com>
2021-08-25 12:45:34 +0100

    vv

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,7 +15,7 @@ class Admin::UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-		  	redirect_to admin_users_path
+		  	root_redirect_to admin_users_path
 		else
 		  	render :new
 		end
