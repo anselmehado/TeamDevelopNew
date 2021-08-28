8e4863eb113a514b40ad26dfa75fed2f8892adf9
hado anselme <anselmehado85@yahoo.com>
2021-08-28 17:03:33 +0100

    mmess

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,4 +1,2 @@
 class Label < ApplicationRecord
-  has_many :labellings, dependent: :destroy
-  has_many :tasks, through: :labellings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_081417) do
+ActiveRecord::Schema.define(version: 2021_08_28_160145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,15 @@ ActiveRecord::Schema.define(version: 2021_08_28_081417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "task_id"
+    t.bigint "label_id"
+    t.index ["label_id"], name: "index_labellings_on_label_id"
     t.index ["task_id"], name: "index_labellings_on_task_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -43,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_08_28_081417) do
     t.boolean "admin"
   end
 
+  add_foreign_key "labellings", "labels"
   add_foreign_key "labellings", "tasks"
   add_foreign_key "tasks", "users"
 end
