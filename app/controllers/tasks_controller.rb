da44a6155ae3bcd080d0d83d890c4bb634d91a92
hado anselme <anselmehado85@yahoo.com>
2021-08-29 15:07:20 +0100

    coo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,15 +4,17 @@ class TasksController < ApplicationController
   # GET /tasks or /tasks.json
   def index
   all_tasks = Task.user_task_list(current_user.id)
-    if params[:task].present?
+
+    if params[:label_id].present?
+      @tasks = all_tasks.label_task_search(params[:label_id]).page params[:page]
+
+    elsif params[:task].present?
       if params[:task][:task_name].present? && params[:task][:status].present?
         @tasks = all_tasks.task_name_fuzzy_search(params[:task][:task_name]).status_search(params[:task][:status]).page params[:page]
       elsif params[:task][:task_name].present?
         @tasks = all_tasks.task_name_fuzzy_search(params[:task][:task_name]).page params[:page]
       elsif params[:task][:status].present?
         @tasks = all_tasks.status_search(params[:task][:status]).page params[:page]
-      elsif params[:label_id].present?
-        @tasks = all_tasks.label_task_search(params[:label_id]).page params[:page]
 
       else
         @tasks = all_tasks.page(params[:page]).per(10)
@@ -30,6 +32,8 @@ class TasksController < ApplicationController
       @tasks = all_tasks.all.page params[:page]
      @tasks = all_tasks.order('task_name').page(params[:page]).per(3)
     end
+
+
   end
 
 
