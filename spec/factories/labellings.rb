b8bf6a194c88a7ba2450815171f922f474a7cdc5
hado anselme <anselmehado85@yahoo.com>
2021-08-31 09:15:31 +0100

    fat

--- spec/factories/labellings.rb
+++ spec/factories/labellings.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :labelling do
-    
+
+    label_id { 2 }
+    task_id { 2 }
+
   end
 end
+
+
+#
+# FactoryBot.define do
+#   factory :labelling do
+#     label_id { 2 }
+#     task_id { 2 }
+#   end
+# end
--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,16 @@
+# FactoryBot.define do
+#   factory :label do
+#     title { "MyString" }
+#   end
+# end
+
+
 FactoryBot.define do
   factory :label do
-    title { "MyString" }
+    title { "Label 1" }
+  end
+
+  factory :label2, class: Label do
+  	title { "Label 2" }
   end
 end
